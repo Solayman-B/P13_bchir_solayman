89100b1815efcd34451e431ec63d33fac1eef722
Solayman <solayman.b@live.fr>
2022-04-23 16:22:48 +0200

    getting database informations clear

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -82,11 +82,11 @@ else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DATABASE_NAME"),
-        "USER": env("DATABASE_USER"),
-        "PASSWORD": env("DATABASE_PASSWORD"),
+        "NAME": "orange_county",#env("DATABASE_NAME"),
+        "USER": "postgres",#env("DATABASE_USER"),
+        "PASSWORD": "password",#env("DATABASE_PASSWORD"),
         "HOST": dbHost,
-        "PORT": env("DATABASE_PORT"),
+        "PORT": "5432",#env("DATABASE_PORT"),
     }
 }
 
